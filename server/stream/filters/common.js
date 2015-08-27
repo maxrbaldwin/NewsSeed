bd65adefc57ad7181db85818cbf45fc7afee869c
Max Baldwin <maxrbaldwin2328@gmail.com>
2015-08-27 19:10:15 -0400

    Changed methods from filter array to for loops for bug issues

--- server/stream/filters/common.js
+++ server/stream/filters/common.js
@@ -1,7 +1,34 @@
 var commonFilters = {
-  isRetweet: function (tweet) {
+  isRetweet: function(keywords) {
+    if(keywords[0] === 'RT') {
+      return keywords;
+    }
+    return false;
+  },
+  filterKeywords: function(keywords) {
+    /**
+    * If a word's length is <= 2 and doesn't start with a capital letter
+    **/
+    var removeRT = keywords.shift();
 
+    return keywords;
+  },
+  getTweetedBy: function(keywords) {
+    for(var i=0; i < keywords.length; i++) {
+      if(keywords[i].substring(0, 1) === '@') {
+        if(keywords[i].slice(-1) === ':') {
+          return keywords[i].substring(0, keywords[i].length - 1);
+        }
+      }
+    }
+  },
+  getTweetLink: function(keywords) {
+    for(var i=0; i < keywords.length; i++) {
+      if(keywords[i].substring(0, 7) === 'http://') {
+        return keywords[i];
+      }
+    }
   }
-};
+ };
 
 module.exports = commonFilters;
