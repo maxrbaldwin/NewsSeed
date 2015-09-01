b6c94b17122cd5ef72d257723ea0fd7939e7f9bc
Max Baldwin <maxrbaldwin2328@gmail.com>
2015-08-31 20:04:58 -0400

    Added routes for data api for tweets stories and seeds. Added routes to index page

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,3 +1,7 @@
 var Router = require('express').Router();
 
+app.use('/seeds', require('./seeds'));
+app.use('/stories', require('./stories'));
+app.use('/tweets', require('./tweets'));
+
 module.exports = Router;
