8f9865722372cac8d5dea5ac5b3507f3329a945e
Max Baldwin <maxrbaldwin2328@gmail.com>
2015-09-03 10:31:42 -0400

    Added cors headers

--- server/app/index.js
+++ server/app/index.js
@@ -11,6 +11,13 @@ app.set('views', __dirname + '/views');
 app.set('view cache', false);
 swig.setDefaults({ cache: false });
 
+// Set cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // All data routes will be prefaced with /api
 app.use('/api', require('./routes'));
 
